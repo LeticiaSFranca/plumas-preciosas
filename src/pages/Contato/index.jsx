bd423f98c902cd82d0504256df4368580ba3fd94
LeticiaSFranca <leticiasilvafranca17@gmail.com>
2024-04-23 21:37:20 -0300

    Criando a página de Contato

--- src/pages/Contato/index.jsx
+++ src/pages/Contato/index.jsx
@@ -1,6 +1,12 @@
 import './Contato.style.css';
 import React from 'react';
 import Cacatua from '../../assets/img/CacatuasContato.png';
+import { FaPaperPlane } from "react-icons/fa6";
+import { IoPersonSharp } from "react-icons/io5";
+import { FaPhone } from "react-icons/fa6";
+import { IoMdMail } from "react-icons/io";
+import { MdMarkunreadMailbox } from "react-icons/md";
+import { AiFillMessage } from "react-icons/ai";
 import { useEffect } from 'react';
 
 export function Contato() {
@@ -12,11 +18,41 @@ export function Contato() {
         <>
             <section id='primeira-apresentacao'>
                 <img src={Cacatua} alt="Cacatua" id='imagem-homepage' />
+            </section>
 
-                <div>
-                    <h1 id='titulo-inicio-cacatuas'>Fale Conosco</h1>
-                    <div></div>
-                </div>
+            <section className="formularioContato">
+                <form action="https://formsubmit.co/leticiasilvafranca17@gmail.com" method="post" encType="multipart/form-data">
+                    <label>
+                        <span><i className="icon icon-user-1"></i><IoPersonSharp /> Nome</span>
+                        <input type="text" name="nome" required placeholder="Seu nome" />
+                    </label>
+
+                    <label>
+                        <span><i className="icon icon-email"></i><IoMdMail /> E-mail</span>
+                        <input type="text" name="email" className="fade_8S" placeholder="exemplo@email.com" />
+                    </label>
+
+                    <label>
+                        <span><i className="icon icon-email"></i><FaPhone /> Telefone</span>
+                        <input type="tel" name="celular" required placeholder="(31)91234-5678" id="telefone" />
+                    </label>
+
+                    <label>
+                        <span><i className="icon icon-flag"></i>< MdMarkunreadMailbox /> Assunto</span>
+                        <input type="text" name="assunto" required placeholder="ex: Orçamento" />
+                    </label>
+
+                    <label>
+                        <span><i className="icon icon-comment" id="caixaMensagem"></i><AiFillMessage /> Mensagem</span>
+                        <textarea name="mensagem" required placeholder="Digite aqui sua mensagem." />
+                    </label>
+
+                    <input type="hidden" name="acao" value="enviar" />
+                    <input type="hidden" name="_captcha" value="false" />
+                    <input type="hidden" name="_next" value="https://plumaspreciosas.vercel.app" />
+                    <button type="submit" className="btn-envia" title="Enviar"><b>Enviar <FaPaperPlane /></b></button>
+                </form>
+                <br />
             </section>
         </>
     );
