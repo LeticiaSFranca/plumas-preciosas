18aacc36fe09dfa061fe9909f8ac328de8fff65c
LeticiaSFranca <leticiasilvafranca17@gmail.com>
2024-04-26 09:29:56 -0300

    Colocando botões na página inicial

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -2,6 +2,8 @@ import { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import React from 'react';
 import Cacatuas from '../../assets/img/CacatuasHomePage.png';
+import BotaoCacatua from '../../assets/img/botao-cacatua.png';
+import BotaoContato from '../../assets/img/botao-contato.png';
 import './Home.style.css';
 
 export function Home() {
@@ -19,6 +21,19 @@ export function Home() {
                     <h2 id='nome-cacatua'>Cacatuas Galerita</h2>
                     <Link to="/contato" id='botao-contato'>Entrar em Contato</Link>
                 </div>
+            </section>
+
+            <section id='segunda-apresentacao'>
+                <Link to='/cacatuas' className='link-botoes-inicio'>
+                    <img src={BotaoCacatua} alt="Filhote de Cacatua" />
+                    Conhecer Cacatuas</Link>
+                <Link to='/contato' className='link-botoes-inicio'>
+                    <img src={BotaoContato} alt="Cacatua no Barril" />
+                    Entrar em Contato</Link>
+            </section>
+
+            <section id="terceira-apresentacao">
+                
             </section>
         </>
     );
